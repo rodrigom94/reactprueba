eab65acc2f33322987522d1feedd4dbd8f6bddc1
Rodrigo <rodrigomelgarejoalegre@gmail.com>
2019-11-01 21:45:20 -0500

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './styles/styles.scss';
 import Curso from './Curso';
 
+const cursos = [
+  {
+    "title":"React desde Cero", 
+    "imagen":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    "prices":30,
+    "profesor": "Beto"
+  },
+  {
+    "title":"Drupal desde Cero", 
+    "imagen":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-10/Vue-JS.png",
+    "prices":110,
+    "profesor": "Alex"
+  },
+  {
+    "title":"HRML desde Cero", 
+    "imagen":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-07/AuditionDesdeCero.png",
+    "prices":300,
+    "profesor": "Alvaro"
+  }
+]
 const App = () => (
+  <>
   <div className="main-banner img-container" id="main-banner">
     <div className="ed-grid lg-grid-6">
       <div className="lg-cols-4 lg-x-2">
@@ -14,8 +35,15 @@ const App = () => (
         </div>
       </div>
     </div>
-    <Curso />
-  </div>
+    </div>
+    <div className="ed-grid m-grid-3">
+      {
+        cursos.map(c=><Curso title={c.title} image={c.imagen} price={c.prices} profesor={c.profesor} />)
+      }
+    </div>  
+
+    </>
+
 )
 
 export default App;
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Curso = () => (
+// const mayorDeEdad = edad => edad > 18;
+// const persona = {"nombre":"Alberto","Apellido":"Quiroga","edad":23}
+
+/** const curso = {
+    "title":"React desde cero",
+    "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    "price":"50usd"
+} */
+
+const Curso = ({title, image, price, profesor}) => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-10/figma-desde-cero-poster.png" alt="" />
+            <img src={image} alt={title} />
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h3 className="t5 s-mb-2 s-center">
-            Programación orientada a objetos con Go
+            <h3 className="center">
+               {title}
             </h3>
-            <div className="s-mb-2 s-main-center">
-            <div className="card__teacher s-cross-center">
-                <div className="card__avatar s-mr-1">
-                <div className="circle img-container">
-                    <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-10/figma-desde-cero-poster.png" alt="#" />
-                </div>
-                </div>
-                <span className="small">Alexys Lozada</span>
-            </div>
-            </div>
             <div className="s-main-center">
-            <a className="button--ghost-alert button--tiny" href="https://ed.team">$ 20USD</a>
+                {`Prof.: ${profesor}`}
+            <a className="button--ghost-alert button--tiny" href="https://ed.team">{`$ ${price}`}</a>
             </div>
         </div>
     </article>
 )
 
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.string,
+    profesor: PropTypes.string
+}
+
+
+Curso.defaultProps = {
+    title: "No se encontró título",
+    image: "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-08/angular-material.png",
+    price: "--",
+    profesor: "No se encontró"
+}
 export default Curso;
